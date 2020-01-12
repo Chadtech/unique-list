a76f1ee57481fe3653212067182114b257569f8b
Luke Joshua <20785216@sun.ac.za>
2020-01-12 23:37:02 +0200

    Add qualified imports and rename all to suite

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,53 +1,52 @@
-module Tests exposing (..)
+module Tests exposing (suite)
 
 import Expect
-import Fuzz exposing (int, list, tuple3)
-import List.Unique exposing (..)
-import Test exposing (..)
+import List.Unique as UniqueList
+import Test exposing (Test, describe, test)
 
 
-all : Test
-all =
+suite : Test
+suite =
     describe "List.Unique"
         [ describe "toList, fromList"
             [ test "list is the same after toList and fromList" <|
                 \() ->
                     Expect.true
                         "(List.Unique.toList <| List.Unique.fromList [ 1, 2, 3 ]) == [ 1, 2, 3 ]"
-                        ((toList <| fromList [ 1, 2, 3 ]) == [ 1, 2, 3 ])
+                        ((UniqueList.toList <| UniqueList.fromList [ 1, 2, 3 ]) == [ 1, 2, 3 ])
             , test "toList removed duplicates" <|
                 \() ->
                     Expect.true
                         "(toList <| fromList [ 1, 1, 2, 3 ]) == [ 1, 2, 3 ]"
-                        ((toList <| fromList [ 1, 1, 2, 3 ]) == [ 1, 2, 3 ])
+                        ((UniqueList.toList <| UniqueList.fromList [ 1, 1, 2, 3 ]) == [ 1, 2, 3 ])
             , test "empty toList is an empty list" <|
                 \() ->
-                    Expect.equal (toList empty) []
+                    Expect.equal (UniqueList.toList UniqueList.empty) []
             , test "remove 'a' from alphabet" <|
                 \() ->
                     Expect.equal
-                        ((toList << remove 'a') (fromList [ 'a', 'b', 'c' ]))
+                        ((UniqueList.toList << UniqueList.remove 'a') (UniqueList.fromList [ 'a', 'b', 'c' ]))
                         [ 'b', 'c' ]
             , test "remove 'b' from alphabet" <|
                 \() ->
                     Expect.equal
-                        ((toList << remove 'b') (fromList [ 'a', 'b', 'c' ]))
+                        ((UniqueList.toList << UniqueList.remove 'b') (UniqueList.fromList [ 'a', 'b', 'c' ]))
                         [ 'a', 'c' ]
             , test "'b' is added after 'a'" <|
                 \() ->
                     Expect.equal
-                        ((toList << ('b' |> addAfter 'a')) (fromList [ 'a', 'c' ]))
+                        ((UniqueList.toList << ('b' |> UniqueList.addAfter 'a')) (UniqueList.fromList [ 'a', 'c' ]))
                         [ 'a', 'b', 'c' ]
             , test "'a' is before 'b'" <|
                 \() ->
-                    Expect.equal (isBefore 'b' 'a' (fromList [ 'a', 'b', 'c' ]))
+                    Expect.equal (UniqueList.isBefore 'b' 'a' (UniqueList.fromList [ 'a', 'b', 'c' ]))
                         (Just True)
             , test "'z' is before 'b' in abc (should be Nothing)" <|
                 \() ->
-                    Expect.equal (isBefore 'b' 'z' (fromList [ 'a', 'b', 'c' ]))
+                    Expect.equal (UniqueList.isBefore 'b' 'z' (UniqueList.fromList [ 'a', 'b', 'c' ]))
                         Nothing
             , test "filterDuplicates [ True, True ] is [ True ]" <|
                 \() ->
-                    Expect.equal (filterDuplicates [ True, True ]) [ True ]
+                    Expect.equal (UniqueList.filterDuplicates [ True, True ]) [ True ]
             ]
         ]
